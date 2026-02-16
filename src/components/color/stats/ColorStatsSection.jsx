29dff77c517679395146b3a06f12c272ccacb89b
juanfeoru <felipeortega383@gmail.com>
2026-02-15 21:54:49 -0500

    feat: Configure VS Code for Tailwind CSS, add slide animations, dynamically display color statistics.

--- src/components/color/stats/ColorStatsSection.jsx
+++ src/components/color/stats/ColorStatsSection.jsx
@@ -1,12 +1,30 @@
 import ColorStatCard from './ColorStatCard';
+import { hexToRgb } from '../../utils/colorUtils';
 
-export default function ColorStatsSection() {
+export default function ColorStatsSection({ color }) {
+  const rgb = hexToRgb(color.hex);
+
   return (
     <section className='w-full max-w-200'>
       <div className='grid grid-cols-3 gap-4'>
-        <ColorStatCard label='RED' value={255} percentage={100} color='red' />
-        <ColorStatCard label='GREEN' value={87} percentage={34} color='green' />
-        <ColorStatCard label='BLUE' value={51} percentage={20} color='blue' />
+        <ColorStatCard
+          label='RED'
+          value={rgb.r}
+          percentage={rgb.r}
+          color='red'
+        />
+        <ColorStatCard
+          label='GREEN'
+          value={rgb.g}
+          percentage={rgb.g}
+          color='green'
+        />
+        <ColorStatCard
+          label='BLUE'
+          value={rgb.b}
+          percentage={rgb.b}
+          color='blue'
+        />
       </div>
     </section>
   );
